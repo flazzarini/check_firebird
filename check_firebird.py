3326293d87798997254907b7741aca751d695cb9
Frank Lazzarini <flazzarini@gmail.com>
2013-06-06 17:04:49 +0200

    PEP fixes

--- check_firebird.py
+++ check_firebird.py
@@ -30,55 +30,75 @@ except ImportError, _:
 import sys, getopt, socket
  
  
-nagios_codes = {'OK': 0, 'WARNING': 1, 'CRITICAL': 2, 'UNKNOWN': 3, 'DEPENDENT': 4}
+NAGIOS_CODE = {'OK': 0,
+               'WARNING': 1,
+               'CRITICAL': 2,
+               'UNKNOWN': 3,
+               'DEPENDENT': 4}
  
  
 def usage():
-    # Returns the nagios status UNKOWN with a usage description
-    nagios_return('UNKNOWN', 'usage: {0} -h <host> -a <alias> -u <username> -p <password> -d <optional destionport default:3050)'.format(sys.argv[0]))
+    """
+    Show Usage
+
+    Returns the nagios status UNKOWN with a usage description
+    """
+    nagios_return('UNKNOWN',
+                  'usage: {0} -h <host> -a <alias> -u <username> -p <password>'
+                  ' -d <optional destionport default:3050)'.format(sys.argv[0]))
  
  
 def nagios_return(code, response):
-    # Prints the response message and exits the script with
-    # one of the defined exit codes.
+    """
+    Prints the response message and exits the script with one of the defined
+    exit codes.
+    """
     print code + ': ' + response
-    sys.exit(nagios_codes[code])
+    sys.exit(NAGIOS_CODE[code])
  
  
 def check_condition(host, destport, alias, username, password):
-    # Tries to connect to the given firebird database and
-    # execute a SQL statement which should return a list
-    # of available tables. Be aware this doesn't work if 
-    # your database has no tables.
-    #
-    #@param host    : hostname/ip
-    #@param alias   : database alias
-    #@param username: username
-    #@param password: password    
- 
+    """
+    Tries to connect to the given firebird database and execute a SQL statement
+    which should return a list of available tables. Be aware this doesn't
+    work if your database has no tables.
+
+    @param host    : hostname/ip
+    @param alias   : database alias
+    @param username: username
+    @param password: password
+    """
     try :
         dsnstring  = host + "/" + destport + ":" + alias
-        connection = fdb.connect(dsn=dsnstring, user=username, password=password)
+        connection = fdb.connect(dsn=dsnstring,
+                                 user=username,
+                                 password=password)
         connection.begin()
         version = connection.server_version
  
         # Execute an sql on the connection
         cur = connection.cursor()
-        cur.execute('SELECT DISTINCT RDB$RELATION_NAME FROM RDB$RELATION_FIELDS WHERE RDB$SYSTEM_FLAG=0;')
+        cur.execute('SELECT DISTINCT RDB$RELATION_NAME FROM RDB$RELATION_FIELDS'
+                    ' WHERE RDB$SYSTEM_FLAG=0;')
  
         if len(cur.fetchall()) < 1:
             connection.close()
-            return {'code': 'CRITICAL', 'message': dsnstring + ' problem with the database, maybe corrupted!'}
+            return {'code': 'CRITICAL',
+                    'message': dsnstring + ' problem with the database,'
+                                           ' maybe corrupted!'}
         connection.close()
  
     except fdb.OperationalError, msg:
-        return {'code': 'CRITICAL', 'message': dsnstring + ' ' + msg[1].rstrip('\n')}
+        return {'code': 'CRITICAL',
+                'message': dsnstring + ' ' + msg[1].rstrip('\n')}
  
     return {'code': 'OK', 'message': host + ' Version:' + version}
  
  
 def is_valid_ipv4_address(address):
-    # Returns true if IP address is in a valid ipv4 format
+    """
+    Returns true if IP address is in a valid ipv4 format
+    """
     try:
         addr = socket.inet_pton(socket.AF_INET, address)
     except AttributeError: # no inet_pton here, sorry
@@ -93,7 +113,9 @@ def is_valid_ipv4_address(address):
     return True
  
 def is_valid_ipv6_address(address):
-    # Returns true if IP address is in a valid ipv6 format
+    """
+    Returns true if IP address is in a valid ipv6 format
+    """
     try:
         addr = socket.inet_pton(socket.AF_INET6, address)
     except socket.error: # not a valid address
@@ -102,15 +124,24 @@ def is_valid_ipv6_address(address):
  
  
 def main():
-    # Check if argument length is not less than 5 which means that 
-    # "-h <host> -a <alias>" must be given as a parameter otherwise
-    # the script will print the usage description using nagios_return()
-    # function, else parse/verify the parameters
+    """
+    Check if argument length is not less than 5 which means that 
+    "-h <host> -a <alias>" must be given as a parameter otherwise
+    the script will print the usage description using nagios_return()
+    function, else parse/verify the parameters
+    """
     if len(sys.argv) < 8:
         usage()
  
     try:
-        opts , args = getopt.getopt(sys.argv[1:], 'h:a:u:p:d:', ['host=', 'pass=', 'alias=', 'user=', 'password=', 'destport='])
+        opts , args = getopt.getopt(sys.argv[1:],
+                                    'h:a:u:p:d:',
+                                    ['host=',
+                                     'pass=',
+                                     'alias=',
+                                     'user=',
+                                     'password=',
+                                     'destport='])
     except getopt.GetoptError:
         usage()
  
@@ -123,7 +154,8 @@ def main():
     for o, value in opts:
         if o in ('-h', '--host'):
             if not is_valid_ipv4_address(value): 
-                nagios_return('UNKNOWN', value + ' is not a valid ipv4 address.'.format(sys.argv[0]))
+                nagios_return('UNKNOWN',
+                              value + ' is not a valid ipv4 address.'.format(sys.argv[0]))
             else :
                 host = value
         elif o in ('-a', '--alias'):
